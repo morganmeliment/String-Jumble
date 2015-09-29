0a74b3655f33a441456ec17bfea2267bd7c884b0
Morgan Meliment <morganmeliment@gmail.com>
2015-09-29 14:52:10 -0400

    Updated from Brython Server: September 29, 2015 2:52:24 PM EDT

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,10 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+sot = input("Please enter a string of text (the bigger the better): ")
+print("You entered " + '"' + sot + '"' + ". Now jumble it:")
+countofsot = len(sot)
+ran = range(countofsot, 0)
+mapped = zip(ran, sot)
+print(list(mapped).sort())
